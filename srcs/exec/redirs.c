e7db357c55fc61b6c4caf5ac9190897cb462a727
Lucas Clodic <lclodic@student.42.fr>
2026-04-15 14:03:50 +0200

    fix pipe toujours cree meme quand pas redir out

--- srcs/exec/redirs.c
+++ srcs/exec/redirs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirs.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucas <lucas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lclodic <lclodic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 14:37:29 by mnicolas          #+#    #+#             */
-/*   Updated: 2026/04/13 17:08:53 by lucas            ###   ########.fr       */
+/*   Updated: 2026/04/15 14:01:44 by lclodic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,27 +53,29 @@ int	close_and_handle_infile(t_node *redirs, t_data *data)
 
 int	create_pipe(t_data *data)
 {
-	if (data->i == data->cmd_count - 1) // ici on regarde si c'est la dernière commande
-		data->outfd = 1;
-	else
+	if (data->i == data->cmd_count - 1)
 	{
-		if (pipe(data->pipefd) == -1)
-		{
-			perror("pipe");
-			return (-1);
-		}
-		data->outfd = data->pipefd[1];
+		if (data->outfd == -2)
+			data->outfd = 1;
+		return (0);
 	}
+	if (pipe(data->pipefd) == -1)
+	{
+		perror("pipe");
+		return (-1);
+	}
+	if (data->outfd == -2)
+		data->outfd = data->pipefd[1];
+	else
+		close(data->pipefd[1]);
 	return (0);
 }
 
 int	open_redirs(t_node *redirs, t_data *data)
 {
 	int	redirs_in;
-	int	redirs_out;
 
 	redirs_in = 0;
-	redirs_out = 0;
 	data->outfd = -2;
 	while (redirs)
 	{
@@ -100,11 +102,7 @@ int	open_redirs(t_node *redirs, t_data *data)
 				data->outfd = open(redirs->str,
 						O_WRONLY | O_TRUNC | O_CREAT, 0644);
 			if (data->outfd == -1)
-			{
-				perror(redirs->str);
-				return (-1);
-			}
-			redirs_out = 1;
+				return (perror(redirs->str), -1);
 		}
 		redirs = redirs->next;
 	}
@@ -120,10 +118,7 @@ int	open_redirs(t_node *redirs, t_data *data)
 		else
 			data->infd = 0;
 	}
-	if (!redirs_out)
-	{
-		if (create_pipe(data) == -1)
-			return (-1);
-	}
+	if (create_pipe(data) == -1)
+		return (-1);
 	return (0);
 }
